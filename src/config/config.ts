7587b43e5b0140de7fa66e39e44282f9e9ce468c
JfreshngSnr <emmanueltimmileyin@gmail.com>
2026-04-26 23:27:12 +0100

    credentials masking.

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const requiredEnv = ["SERVER_NAME", "DB_NAME", "DB_PORT", "DB_USERNAME", "DB_PASSWORD"];
+
+// requiredEnv.forEach((key) => {
+//   if (!process.env[key]) {
+//     throw new Error(`Missing env variable: ${key}`);
+//   }
+// });
+
 export const appConfig = {
-  db: process.env.DATABASE_URL
+  db_url: process.env.DATABASE_URL,
+  server_name: process.env.SERVER_NAME,
+  db_name: process.env.DB_NAME,
+  db_port: process.env.DB_PORT,
+  db_username: process.env.DB_USERNAME,
+  db_password: process.env.DB_PASSWORD,
 }
